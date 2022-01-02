f3a6316f0055a71415975e290c865c481a86191b
Max Stein <maxwell.stein@gmail.com>
2022-01-02 12:18:53 -0800

    adds base files

---
+++ example/.eslintrc.js
@@ -0,0 +1,28 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    node: true
+  },
+  parserOptions: {
+    parser: 'babel-eslint'
+  },
+  extends: ['@nuxtjs', 'plugin:nuxt/recommended'],
+  plugins: ['vue', 'prettier'],
+  // add your custom rules here
+  rules: {
+    'vue/max-attributes-per-line': 'off',
+    'arrow-parens': 'off',
+    'space-before-function-paren': [
+      'error',
+      {
+        anonymous: 'always',
+        named: 'never',
+        asyncArrow: 'always'
+      }
+    ],
+    'vue/html-self-closing': 'off',
+    'vue/multiline-html-element-content-newline': 'off',
+    'vue/singleline-html-element-content-newline': 'off'
+  }
+}
